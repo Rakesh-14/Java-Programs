af94b7b1ff6582dca11b27e11173e72113de670b
Rakesh-14 <72142954+Rakesh-14@users.noreply.github.com>
2020-10-01 10:54:49 +0530

    Added the Factorial program

---
+++ Factorial.java
@@ -0,0 +1,12 @@
+# https://www.facebook.com/mayankgbrc/posts/2726954114288546
+# Subscribed by Code House
+class FactorialExample{  
+ public static void main(String args[]){  
+  int i,fact=1;  
+  int number=5;//It is the number to calculate factorial    
+  for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  System.out.println("Factorial of "+number+" is: "+fact);    
+ }  
+}  
